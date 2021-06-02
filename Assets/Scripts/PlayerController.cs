731b6d8961e2e9535f2db6ac107beb0b7ea9163b
Rersome <44253017+Rersome@users.noreply.github.com>
2021-06-03 03:03:50 +1000

    //

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : PhysicsObject
 {
+
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
@@ -18,23 +19,23 @@ public class PlayerController : PhysicsObject
 
     protected override void ComputeVelocity()
     {
-
         Vector2 move = Vector2.zero;
 
         move.x = Input.GetAxis("Horizontal");
 
-        if(Input.GetButtonDown ("Jump") && grounded)
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             velocity.y = jumpTakeOffSpeed;
-        } else if (Input.GetButtonUp("Jump"))
+        }
+        else if (Input.GetButtonUp("Jump"))
         {
-            if(velocity.y > 0)
+            if (velocity.y > 0)
             {
-                velocity.y = velocity.y * 0.5f;
+                velocity.y = velocity.y * 0.8f;
             }
         }
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.1f));
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
         if (flipSprite)
         {
             spriteRenderer.flipX = !spriteRenderer.flipX;
@@ -45,5 +46,4 @@ public class PlayerController : PhysicsObject
 
         targetVelocity = move * maxSpeed;
     }
-
 }
