d2ea2fbe203d2d31bf33713b3761851ea76e276e
Rersome <44253017+Rersome@users.noreply.github.com>
2021-06-24 19:41:16 +1000

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,45 +5,102 @@ using UnityEngine;
 public class PlayerController : PhysicsObject
 {
 
-    public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
 
-    private SpriteRenderer spriteRenderer;
-    //private Animator animator;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private GroundDetector m_groundSensor;
+    private bool m_grounded = false;
+    private bool m_combatIdle = false;
+    private bool m_isDead = false;
 
-    void Awake()
+    // Use this for initialization
+    void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        //animator = GetComponent<Animator>();
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<GroundDetector>();
     }
 
-    protected override void ComputeVelocity()
+    // Update is called once per frame
+    void Update()
     {
-        Vector2 move = Vector2.zero;
-
-        move.x = Input.GetAxis("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        //Check if character just landed on the ground
+        if (!m_grounded && m_groundSensor.State())
         {
-            velocity.y = jumpTakeOffSpeed;
-        }
-        else if (Input.GetButtonUp("Jump"))
-        {
-            if (velocity.y > 0)
-            {
-                velocity.y = velocity.y * 0.3f;
-            }
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
         }
 
-        bool flipSprite = ((spriteRenderer.flipX) ? (move.x < 0.01f) : (move.x > 0.01f));
-        if (flipSprite)
+        //Check if character just started falling
+        if (m_grounded && !m_groundSensor.State())
         {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
         }
 
-        //animator.SetBool("grounded", grounded);
-        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        // -- Handle input and movement --
+        float inputX = Input.GetAxis("Horizontal");
 
-        targetVelocity = move * maxSpeed;
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (inputX < 0)
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Move
+        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+
+        //Set AirSpeed in animator
+        m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
+
+        // -- Handle Animations --
+        //Death
+        if (Input.GetKeyDown("e"))
+        {
+            if (!m_isDead)
+                m_animator.SetTrigger("Death");
+            else
+                m_animator.SetTrigger("Recover");
+
+            m_isDead = !m_isDead;
+        }
+
+        //Hurt
+        else if (Input.GetKeyDown("q"))
+            m_animator.SetTrigger("Hurt");
+
+        //Attack
+        else if (Input.GetMouseButtonDown(0))
+        {
+            m_animator.SetTrigger("Attack");
+        }
+
+        //Change between idle and combat idle
+        else if (Input.GetKeyDown("f"))
+            m_combatIdle = !m_combatIdle;
+
+        //Jump
+        else if (Input.GetKeyDown("space") && m_grounded)
+        {
+            m_animator.SetTrigger("Jump");
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            m_groundSensor.Disable(0.2f);
+        }
+
+        //Run
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+            m_animator.SetInteger("AnimState", 2);
+
+        //Combat Idle
+        else if (m_combatIdle)
+            m_animator.SetInteger("AnimState", 1);
+
+        //Idle
+        else
+            m_animator.SetInteger("AnimState", 0);
     }
 }
